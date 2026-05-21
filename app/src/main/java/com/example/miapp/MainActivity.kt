94ff38dc465c661ea8ad6f7812339b08a99c79f3
walking05studio <walking05studios@gmail.com>
2026-05-21 01:10:53 -0600

    Cambio realizado en rama1

--- app/src/main/java/com/example/miapp/MainActivity.kt
+++ app/src/main/java/com/example/miapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MiappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Rama 1",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
